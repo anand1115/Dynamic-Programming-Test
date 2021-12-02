e208a90ae476114eea8fd532efa65647247eb81b
anand1115 <anandpasam1115@gmail.com>
2021-12-02 21:57:35 +0530

    update

--- Test.py
+++ Test.py
@@ -4,6 +4,8 @@ class Node:
         self.count=0
         self.locked=False
         self.parent=None
+        self.locked_list={}
+        self.locked_id=None
 
     def __repr__(self):
         return str(self.key)
@@ -12,7 +14,7 @@ class Tree:
     def __init__(self):
         self.map={}
 
-    def lock(self,node):
+    def lock(self,node,uid):
         if(node.locked or node.count!=0):
             return False
         else:
@@ -22,22 +24,47 @@ class Tree:
                     return False
                 temp=temp.parent
             node.locked=True
+            node.locked_id=uid
             temp=node.parent
             while temp:
                 temp.count+=1
+                temp.locked_list[id(node)]=node
                 temp=temp.parent
             return True
 
-    def unlock(self,node):
+    def unlock(self,node,uid):
         if not node.locked:
+            return False
+        elif(node.locked_id!=uid):
             return False
         else:
             temp=node.parent
             while temp:
                 temp.count-=1
+                del temp.locked_list[id(node)]
                 temp=temp.parent
             node.locked=False
+            node.locked_id=None
             return True
+
+    def upgrade(self,node,uid):
+        if(node.locked):
+            return True
+        temp=node.parent
+        while temp:
+            if(temp.locked):
+                return False
+            temp=temp.parent
+        temp=list(node.locked_list.values()).copy()
+        for i in temp:
+            if(i.locked_id!=uid):
+                return False
+        for i in temp:
+            self.unlock(i,uid)
+        self.lock(node,uid)
+        return True
+                
+        
 
 
 n=int(input())
@@ -63,8 +90,11 @@ q=int(input())
 for j in range(q):
     query=input().split()
     if(query[0]=='1'):
-        print(tree.lock(tree.map[query[1]]))
+        print(tree.lock(tree.map[query[1]],query[2]))
+    elif(query[0]=='2'):
+        print(tree.unlock(tree.map[query[1]],query[2]))
     else:
-        print(tree.unlock(tree.map[query[1]]))        
+        print(tree.upgrade(tree.map[query[1]],query[2]))
+    
     for i in arr:
-        print(i,"-->",i.count,'-->',i.locked)
+        print(i,"-->",i.count,'-->',i.locked,'-->',i.locked_list)
